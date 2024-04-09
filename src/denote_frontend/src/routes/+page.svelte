45095757ab60cb1490f0706b68d753fded92e311
vkabadzhova <vkabadzhova@gmail.com>
2024-04-09 19:01:43 +0100

    Fix frontend - add Juno Svetle example

--- src/denote_frontend/src/routes/+page.svelte
+++ src/denote_frontend/src/routes/+page.svelte
@@ -1,26 +1,68 @@
-<script>
-  import "../index.scss";
-  import { backend } from "$lib/canisters";
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+	import { authSubscribe, initJuno, type User } from '@junobuild/core';
+	import Logo from '$lib/assets/juno_logo_white.svg';
+	import Main from '$lib/components/Main.svelte';
+	import { userStore } from '$lib/stores/user.store';
+	import SignOut from '$lib/components/SignOut.svelte';
 
-  let greeting = "";
+	let unsubscribe: (() => void) | undefined = undefined;
 
-  function onSubmit(event) {
-    const name = event.target.name.value;
-    backend.greet(name).then((response) => {
-      greeting = response;
-    });
-    return false;
-  }
+	onMount(async () => {
+		unsubscribe = authSubscribe((user: User | null) => userStore.set(user));
+
+		await initJuno({
+			satelliteId: 'jx5yt-yyaaa-aaaal-abzbq-cai'
+		});
+	});
+
+	onDestroy(() => unsubscribe?.());
 </script>
 
 <main>
-  <img src="/logo2.svg" alt="DFINITY logo" />
-  <br />
-  <br />
-  <form action="#" on:submit|preventDefault={onSubmit}>
-    <label for="name">Enter your name: &nbsp;</label>
-    <input id="name" alt="Name" type="text" />
-    <button type="submit">Click Me!</button>
-  </form>
-  <section id="greeting">{greeting}</section>
+	<h1><img src={Logo} role="presentation" loading="lazy" decoding="async" alt="Juno" /></h1>
+	<p>
+		Sign-up to join <a href="https://juno.build" target="_blank" rel="noreferrer noopener">Juno</a> closed
+		beta.
+	</p>
+
+	<section>
+		<Main />
+	</section>
+
+	<SignOut />
 </main>
+
+<style lang="scss">
+	@use '../lib/styles/mixins/media';
+	@use '../lib/styles/mixins/shadow';
+
+	main {
+		max-width: media.$breakpoint-large;
+		padding: var(--padding-4x) var(--padding-2x);
+
+		@include media.min-width(large) {
+			margin: var(--padding-8x) auto 0;
+		}
+	}
+
+	h1 {
+		font-size: 3rem;
+		width: 50%;
+	}
+
+	p {
+		margin: 0 0 var(--padding-2x);
+	}
+
+	section {
+		margin: var(--padding-4x) 0;
+		padding: var(--padding-2x);
+
+		@include shadow.card;
+
+		@include media.min-width(large) {
+			padding: var(--padding-4x);
+		}
+	}
+</style>
--- src/denote_frontend/svelte.config.js
+++ src/denote_frontend/svelte.config.js
@@ -1,19 +1,25 @@
 import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      pages: 'dist',
-      assets: 'dist',
-      fallback: undefined,
-      precompress: false,
-      strict: true,
-    }),
-  },
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: preprocess({
+		postcss: {
+			plugins: [autoprefixer]
+		}
+	}),
+
+	kit: {
+		adapter: adapter({
+			precompress: true
+		})
+	},
+	serviceWorker: {
+		register: false
+	}
 };
 
 export default config;
